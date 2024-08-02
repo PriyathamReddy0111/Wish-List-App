1d41a0ad88069bb2959db1405cccbf9e2d3fe7bb
Priyatham Reddy <priyathamreddy106@gmail.com>
2024-08-02 21:35:42 +0530

    Adding a Navigation Icon to our AppBar

--- app/src/main/java/com/example/wishlistapp/AppBar.kt
+++ app/src/main/java/com/example/wishlistapp/AppBar.kt
@@ -1,11 +1,13 @@
 package com.example.wishlistapp
 
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Text
-
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
@@ -14,17 +16,28 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun AppBarView(
     title: String,
-    onBackNavClicked: () -> Unit={}
-){
-    TopAppBar(title = { Text(text = title,
-        color = colorResource(id = R.color.white),
-        modifier = Modifier
-            .padding(start = 4.dp)
-            .heightIn(max = 24.dp)
-    ) },
-        elevation = 3.dp,
-        backgroundColor = colorResource(id = R.color.black)
+    onBackNavClicked: () -> Unit = {}
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                color = colorResource(id = R.color.white),
+                modifier = Modifier
+                    .padding(start = 4.dp)
+                    .heightIn(max = 24.dp)
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = { onBackNavClicked() }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    tint = colorResource(id = R.color.white),
+                    contentDescription = null
+                )
+            }
+        },
+        backgroundColor = colorResource(id = R.color.black),
+        elevation = 3.dp
     )
 }
-
-
