67e32f909ebcf1cef2e439ce18f5f18b8a9da5c2
Priyatham Reddy <priyathamreddy106@gmail.com>
2024-08-03 12:15:31 +0530

    Floating Action Button

--- app/src/main/java/com/example/wishlistapp/HomeView.kt
+++ app/src/main/java/com/example/wishlistapp/HomeView.kt
@@ -5,18 +5,40 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeView(){
     val context = LocalContext.current
-    Scaffold(topBar = {AppBarView(title = "Wish List",{
-        Toast.makeText(context, "Button Clicked",Toast.LENGTH_LONG).show()
-    })}){
-        LazyColumn(modifier = Modifier.fillMaxSize().padding(it)){
+    Scaffold(
+        topBar = {AppBarView(title = "Wish List",{
+            Toast.makeText(context, "Button Clicked",Toast.LENGTH_LONG).show()
+        })},
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(all =20.dp),
+                onClick = { Toast.makeText(context, "FAButton Clicked",Toast.LENGTH_LONG).show() },
+                contentColor = Color.White,
+                backgroundColor = Color.Blue
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        }
+
+
+    ){
+        LazyColumn(modifier = Modifier
+            .fillMaxSize()
+            .padding(it)){
 
         }
     }
